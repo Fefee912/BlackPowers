10facb5c89b6d283287c4af267d4ed2e9c0ca3ff
Felipe Lopez <horiartz12355@gmail.com>
2024-02-06 05:52:52 -0300

    Header Terminado

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,3 +6,19 @@ export default {
 	},
 	plugins: [],
 }
+
+{
+	"animation"; {
+	  "background-shine"; "background-shine 2s linear infinite"
+	}
+	"keyframes"; {
+	  "background-shine"; {
+		"from"; {
+		  "backgroundPosition"; "0 0"
+		}
+		"to"; {
+		  "backgroundPosition"; "-200% 0"
+		}
+	  }
+	}
+  }
